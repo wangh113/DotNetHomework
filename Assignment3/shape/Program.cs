ae7f8c852d0c4cd58d4cd11afbacb22217232563
wangh113 <126656684+wangh113@users.noreply.github.com>
2023-03-14 20:42:46 +0800

    Add files via upload

---
+++ Assignment3/shape/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape
+{
+    public abstract class Shape
+    {
+        public abstract double GetArea();
+        public abstract bool IsLegal();
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public override bool IsLegal()
+        {
+            return Length > 0 && Width > 0 && Length > Width ;
+        }
+        
+        public override double GetArea()
+        {
+            return Length * Width;
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public double Side
+        {
+            get => Length;
+            set => Length = Width = value;
+        }
+
+        public override bool IsLegal()
+        {
+            return Length > 0 ;
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+        public override double GetArea()
+        {
+            double p = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+        }
+
+        public override bool IsLegal()
+        {
+            return SideA + SideB > SideC && SideA + SideC > SideB && SideB + SideC > SideA;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = new Shape[] {
+            new Rectangle { Length = 5, Width = 2 },
+            new Rectangle { Length = 2, Width = 5 },
+            new Square { Side = 4 },
+            new Square { Side = -3 },
+            new Triangle { SideA = 3, SideB = 4, SideC = 5 },
+            new Triangle { SideA = 3, SideB = 4, SideC = 7 },
+        };
+
+            foreach (var shape in shapes)
+            {
+                if (shape.IsLegal())
+                {
+                    Console.WriteLine($"isLegal,Area: {shape.GetArea()}.");
+                }
+                else
+                {
+                    Console.WriteLine("illegal.");
+                }
+            }
+        }
+    }
+}
---
+++ Assignment3/totalshape/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace totalshape
+{
+    public abstract class Shape
+    {
+        public abstract int GetArea();
+    }
+    public class Rectangle : Shape
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public override int GetArea()
+        {
+            return Length * Width;
+        }
+    }
+
+    public class Square : Shape
+    {
+        public int Side { get; set; }
+        public override int GetArea()
+        {
+            return Side * Side;
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public static Triangle NextTriangle()
+        {
+            Random rnd = new Random();
+            int a = Convert.ToInt32(rnd.NextDouble() * 10);
+            int b = Convert.ToInt32(rnd.NextDouble() * 10);
+            int c = Convert.ToInt32(rnd.NextDouble() * 10);
+
+            while (!IsValidTriangle(a, b, c))
+            {
+                a = Convert.ToInt32(rnd.NextDouble() * 10);
+                b = Convert.ToInt32(rnd.NextDouble() * 10);
+                c = Convert.ToInt32(rnd.NextDouble() * 10);
+            }
+
+            return new Triangle(a, b, c);
+        }
+
+        private static bool IsValidTriangle(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                return false;
+            }
+
+            if (a + b <= c || a + c <= b || b + c <= a)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    public static class RandomHelper
+    {
+        static Random rnd = new Random();
+
+        public static int Next()
+        {
+            return rnd.Next(1,10);
+        }
+
+        public static string NextShapeType()
+        {
+            string[] types = { "rectangle", "square", "triangle" };
+            return types[rnd.Next(types.Length)];
+        }
+    }
+
+    public class ShapeFactory
+    {
+        public static Shape CreateShape(string type)
+        {
+            switch (type)
+            {
+                case "rectangle":
+                    return new Rectangle { Length = RandomHelper.Next(), Width = RandomHelper.Next() };
+                case "square":
+                    return new Square { Side = RandomHelper.Next() };
+                case "triangle":
+                    return new Triangle { SideA = RandomHelper.Next(), SideB = RandomHelper.Next(), SideC = RandomHelper.Next() };
+                default:
+                    throw new ArgumentException($"Invalid shape type: {type}");
+            }
+        }
+    }
+
+    class Program
+    {
+        const int ShapeCount = 10;
+
+        static void Main(string[] args)
+        {
+            double totalArea = 0;
+            for (int i = 0; i < ShapeCount; i++)
+            {
+                var shape = ShapeFactory.CreateShape(RandomHelper.NextShapeType());
+                Console.WriteLine($"Created a {shape.GetType().Name} with area {shape.GetArea()}");
+                totalArea += shape.GetArea();
+            }
+            Console.WriteLine($"Total area: {totalArea}");
+        }
+    }
+}
