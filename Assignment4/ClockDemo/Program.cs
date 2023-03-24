b3679ea6b00f1bc08fb456b212369455717cef1a
wangh113 <126656684+wangh113@users.noreply.github.com>
2023-03-24 11:28:58 +0800

    Add files via upload

---
+++ Assignment4/ClockDemo/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockDemo
+{
+    public delegate void TickEventHandler();
+    public delegate void AlarmEventHandler();
+
+    class Clock
+    {
+        private int _hour;
+        private int _minute;
+        public event TickEventHandler Tick;
+        public event AlarmEventHandler Alarm;
+
+        public Clock(int hour, int minute)
+        {
+            _hour = hour;
+            _minute = minute;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                DateTime currentTime = DateTime.Now;
+                if (currentTime.Hour == _hour && currentTime.Minute == _minute)
+                {
+                    Alarm?.Invoke();
+                    break;
+                }
+                else
+                {
+                    if (Tick != null)
+                    {
+                        Tick();
+                    }
+                    System.Threading.Thread.Sleep(1000); // 等待1秒
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建闹钟实例，设置时间为 11:20
+            Clock clock = new Clock(11, 20);
+            // 订阅嘀嗒事件和响铃事件
+            clock.Tick += new TickEventHandler(OnTick);
+            clock.Alarm += new AlarmEventHandler(OnAlarm);
+            // 运行闹钟
+            clock.Run();
+        }
+
+        // 嘀嗒事件处理函数
+        static void OnTick()
+        {
+            Console.WriteLine("滴答,当前时间："+ DateTime.Now.ToString());
+        }
+
+        // 响铃事件处理函数
+        static void OnAlarm()
+        {
+            Console.WriteLine("响铃,闹钟为：" + DateTime.Now.ToString());
+        }
+    }
+}
---
+++ Assignment4/GenericList/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> doubleList = new List<double>() { 1.5, 2.3, 3.6, 4.2, 5.1 };
+            doubleList.ForEach(d => Console.WriteLine($"Double number: {d}"));
+
+            double max = Double.MinValue;
+            double min = Double.MaxValue;
+            double sum = 0.0;
+            doubleList.ForEach(d =>
+            {
+                if (d > max)
+                {
+                    max = d;
+                }
+                if (d < min)
+                {
+                    min = d;
+                }
+                sum += d;
+            });
+            Console.WriteLine($"Max: {max}, Min: {min}, Sum: {sum}");
+        }
+    }
+
+    public class MyList<T>
+    {
+        private List<T> items;
+
+        public MyList()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            foreach (T item in items)
+            {
+                action(item);
+            }
+        }
+    }
+}
